9da68dbffb19a17962bc63d6daace3a0a9ea5f96
Abhishek Pardhiya <abhishekpardhiya@gmail.com>
2024-12-10 14:12:02 +0530

    major changes

--- BSS-frontend/src/app/greet-page/greet-page.component.ts
+++ BSS-frontend/src/app/greet-page/greet-page.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-greet-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,RouterModule],
   templateUrl: './greet-page.component.html',
   styleUrl: './greet-page.component.css'
 })
-export class GreetPageComponent {
+export class GreetPageComponent implements OnInit {
+
+  constructor(private router:Router){}
+
+  ngOnInit(){
+        // Redirect to home after 20 seconds
+  setTimeout(() => {
+    this.router.navigate(['/']);
+  }, 2000); 
+  }
 
 }
