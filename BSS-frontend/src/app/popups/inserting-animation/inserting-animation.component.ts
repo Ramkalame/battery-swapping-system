17011046075726d07f06469002f245723d4c952f
Ramkalame <ramkalame14082001@gmail.com>
2024-12-14 15:43:06 +0530

    some

--- BSS-frontend/src/app/popups/inserting-animation/inserting-animation.component.ts
+++ BSS-frontend/src/app/popups/inserting-animation/inserting-animation.component.ts
@@ -16,10 +16,13 @@ export class InsertingAnimationComponent implements OnInit {
   openDoor: number = 0; // This will store which box is open
   isTakingBatteryAnimationShow:boolean=false;
   isInsertingBatteryAnimationShow:boolean=true;
+  activeStep = 1; // Start with step-1
+  private intervalId: any;
   constructor(private openBoxSignalService:OpenBoxSignalService){}
 
   ngOnInit(): void {
     this.fetchBoxStatus();
+    this.startAnimationSequence();
   }
 
   fetchBoxStatus(){
@@ -30,6 +33,14 @@ export class InsertingAnimationComponent implements OnInit {
   }
   closePopup(): void {
     this.close.emit(); // Emit an event to close the popup
+  }
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId); // Clear interval to avoid memory leaks
+  }
+  startAnimationSequence(): void {
+    this.intervalId = setInterval(() => {
+      this.activeStep = this.activeStep < 3 ? this.activeStep + 1 : 1; // Loop through steps
+    }, 15000); // Change step every 15 seconds
   }
 
 }
